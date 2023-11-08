468955437fbed24a257a9634d7482d70ff2c7940
Yogabayu <yogabayusbi@gmail.com>
2023-11-08 12:00:42 +0700

    update

--- resources/views/components/auth-footer.blade.php
+++ resources/views/components/auth-footer.blade.php
@@ -1,3 +1,3 @@
   <div class="simple-footer">
-      Copyright &copy; Stisla 2018
+      Copyright &copy; <span id="currentYear"></span> - PT BPR Arthaya Indotama Pusaka
   </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,12 +39,12 @@
                     </div>
                 </div>
 
-                <div class="form-group">
+                {{-- <div class="form-group">
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
                         <label class="custom-control-label" for="remember-me">Remember Me</label>
                     </div>
-                </div>
+                </div> --}}
 
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
@@ -52,31 +52,15 @@
                     </button>
                 </div>
             </form>
-            <div class="mt-4 mb-3 text-center">
-                <div class="text-job text-muted">Login With Social</div>
-            </div>
-            <div class="row sm-gutters">
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                        <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                </div>
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                        <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                </div>
-            </div>
-
         </div>
-    </div>
-    <div class="text-muted mt-5 text-center">
-        Don't have an account? <a href="auth-register.html">Create One</a>
     </div>
 @endsection
 
 @push('scripts')
     <!-- JS Libraies -->
-
+    <script>
+        const currentYear = new Date().getFullYear();
+        document.getElementById('currentYear').textContent = currentYear;
+    </script>
     <!-- Page Specific JS File -->
 @endpush
