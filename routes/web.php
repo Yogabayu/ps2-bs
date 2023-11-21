0b32043465e21aff10247a4eba60fa19ff414664
Yogabayu <yogabayusbi@gmail.com>
2023-11-21 16:39:30 +0700

    update signin form

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('/', [AuthController::class, 'index']);
     Route::post('login', [AuthController::class, 'login'])->name('login');
+    
 });
 
 // admin
