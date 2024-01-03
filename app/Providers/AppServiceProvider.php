1499f1f817f5b7ad5e868f4ff975ff8a55385d8a
Yogabayu <yogabayusbi@gmail.com>
2024-01-03 11:41:26 +0700

    tes update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFour();
-        Schema::defaultStringLength(125);
+        // Schema::defaultStringLength(250);
     }
 }
